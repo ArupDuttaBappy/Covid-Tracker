ce3a06bd7dac70c213f1b79a825aa7ecec7645d4
Arup Dutta Bappy <ad.bappy10@gmail.com>
2022-05-01 23:33:50 +0600

    async await fetch

--- script/app.js
+++ script/app.js
@@ -1,13 +1,21 @@
-var requestOptions = {
-  method: 'GET',
-  redirect: 'follow'
-};
+let country_table_row = document.getElementById('country_list_table_body');
 
-fetch("https://api.covid19api.com/summary", requestOptions)
+
+//  Country List Tab starts
+
+let country_page;
+let country_list_data;
+let sortable_country_matrix = new Array(194).fill(0).map(() => new Array(7).fill(0));
+
+async function fetch_api_country_summary_data(){
+  let requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  let local = await fetch("https://api.covid19api.com/summary", requestOptions)
   .then(response => response.json())
   .then(data => {
     const total_countries = data.Countries.length;
-    let sortable_country_matrix = new Array(total_countries).fill(0).map(() => new Array(7).fill(0));
     let i = 0;
     data.Countries.forEach(data => {
       sortable_country_matrix[i][0] = data.Country;
@@ -29,7 +37,15 @@ fetch("https://api.covid19api.com/summary", requestOptions)
         return ((a[1]+a[3]) < (b[1]+b[3])) ? -1 : 1;
       }
     });
-    console.log('data', data);
-    console.log('sortable_country_matrix', sortable_country_matrix)
   })
   .catch(error => console.log('error', error));
+
+  return sortable_country_matrix;
+}
+
+async function start() {
+  country_list_data = await fetch_api_country_summary_data();
+}
+start();
+
+//  Country List Tab ends
